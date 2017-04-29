9d327b22484c37173ea88fdb7efd16d5abb5bc17
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-04-29 00:18:38 -0300

    Módulo 02 - Aula 04.01

--- Modulo01/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ Modulo01/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,51 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        private string _descricao;
-        public string Descricao
-        {
-            get { return _descricao; }
-            set { SetProperty(ref _descricao, value); }
-        }
+        private string _searchTerm;
 
-        private decimal _valor;
-
-        public decimal Valor
+        public string SearchTerm
         {
-            get { return _valor; }
+            get { return _searchTerm; }
             set
             {
-                SetProperty(ref _valor, value);
+                if (SetProperty(ref _searchTerm, value))
+                    SearchCommand.ChangeCanExecute();
             }
         }
 
+        public Command SearchCommand { get; }
+
         public MainViewModel()
         {
-            Descricao = "Olá mundo! Eu estou aqui!";
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+        }
 
-            Valor = 100.25M;
+        private bool CanExecuteSearchCommand(object arg)
+        {
+            return !string.IsNullOrWhiteSpace(SearchTerm);
+        }
 
-            Task.Delay(3000).ContinueWith(async t =>
+        async private void ExecuteSearchCommand(object obj)
+        {
+            await Task.Delay(2000);
+            bool resposta = await App.Current.MainPage.DisplayAlert("MonkeyHubApp",
+                $"Você pesquisa por '{SearchTerm}'?", "Sim", "Não");
+
+            if (resposta)
             {
-                Descricao = "Meu texto mudou";
-
-                for (int i = 0; i < 10; i++)
-                {
-                    await Task.Delay(1000);
-
-                    Descricao = $"Meu texto mudou{i}";
-                }
-                
-            });
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Obrigado.", "Ok");
+            }
+            else await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"De nada.", "Ok");
         }
     }
 }
