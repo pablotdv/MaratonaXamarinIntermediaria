3daaffb5fe7c6ad2da215e5b7037e0d46c1862d8
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-05-01 11:44:28 -0300

    Módulo 02 - Lab02

---
+++ Modulo02/Lab02/Lab02/Lab02/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Lab02.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ Modulo02/Lab02/Lab02/Lab02/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _propriedadeTexto = "Texto inicial";
+
+        public string PropriedadeTexto
+        {
+            get { return _propriedadeTexto; }
+            set
+            {
+                _propriedadeTexto = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
